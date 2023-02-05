6da2df6908414d5e03187d0b783bb318089acef4
Adam Bogocz <adi_bog@o2.pl>
2023-02-05 17:40:49 +0100

    changes as recommended by Michał

--- js/script.js
+++ js/script.js
@@ -1,58 +1,58 @@
 console.log("Cześć!");
 
-let section__buttonEducation = document.querySelector(".js-section__buttonEducation");
-let section__buttonEducationName = document.querySelector(".js-section__buttonEducationName");
-let section__buttonCourses = document.querySelector(".js-section__buttonCourses");
-let section__buttonCoursesName = document.querySelector(".js-section__buttonCoursesName")
-let section__buttonSkils = document.querySelector(".js-section__buttonSkils");
-let section__buttonSkilsName = document.querySelector(".js-section__buttonSkilsName");
-let section__buttonInterested = document.querySelector(".js-section__buttonInterested");
-let section__buttonInterestedName = document.querySelector(".js-section__buttonInterestedName");
-let section__education = document.querySelector(".section__education");
-let section__courses = document.querySelector(".section__courses");
-let section__skils = document.querySelector(".section__skils");
-let section__interested = document.querySelector(".section__interested");
+let educationButtonElement = document.querySelector(".js-educationButton");
+let educationButtonName = document.querySelector(".js-educationButtonName");
+let coursesButtonElement = document.querySelector(".js-coursesButton");
+let coursesButtonName = document.querySelector(".js-coursesButtonName")
+let skilsButtonElement = document.querySelector(".js-skilsButton");
+let skilsButtonName = document.querySelector(".js-skilsButtonName");
+let interestedButtonElement = document.querySelector(".js-interestedButton");
+let interestedButtonName = document.querySelector(".js-interestedButtonName");
+let education = document.querySelector(".article--education");
+let courses = document.querySelector(".article--courses");
+let skils = document.querySelector(".article--skils");
+let interested = document.querySelector(".article--interested");
 
-section__buttonEducation.addEventListener("click", () => {
-    section__education.classList.toggle("section__hidden");
-    section__courses.classList.add("section__hidden");
-    section__skils.classList.add("section__hidden");
-    section__interested.classList.add("section__hidden");
-    section__buttonEducationName.innerText = section__education.classList.contains("section__hidden") ? "Education" : "Ukryj";
-    section__buttonCoursesName.innerText = section__courses.classList.contains("section__hidden") ? "Courses" : "Ukryj";
-    section__buttonSkilsName.innerText = section__skils.classList.contains("section__hidden") ? "Skils" : "Ukryj";
-    section__buttonInterestedName.innerText = section__interested.classList.contains("section__hidden") ? "Interested" : "Ukryj";
+educationButtonElement.addEventListener("click", () => {
+    education.classList.toggle("article--hidden");
+    courses.classList.add("article--hidden");
+    skils.classList.add("article--hidden");
+    interested.classList.add("article--hidden");
+    educationButtonName.innerText = education.classList.contains("article--hidden") ? "Education" : "Ukryj";
+    coursesButtonName.innerText = courses.classList.contains("article--hidden") ? "Courses" : "Ukryj";
+    skilsButtonName.innerText = skils.classList.contains("article--hidden") ? "Skils" : "Ukryj";
+    interestedButtonName.innerText = interested.classList.contains("article--hidden") ? "Interested" : "Ukryj";
 });
 
-section__buttonCourses.addEventListener("click", () => {
-    section__education.classList.add("section__hidden");
-    section__courses.classList.toggle("section__hidden");
-    section__skils.classList.add("section__hidden");
-    section__interested.classList.add("section__hidden");
-    section__buttonEducationName.innerText = section__education.classList.contains("section__hidden") ? "Education" : "Ukryj";
-    section__buttonCoursesName.innerText = section__courses.classList.contains("section__hidden") ? "Courses" : "Ukryj";
-    section__buttonSkilsName.innerText = section__skils.classList.contains("section__hidden") ? "Skils" : "Ukryj";
-    section__buttonInterestedName.innerText = section__interested.classList.contains("section__hidden") ? "Interested" : "Ukryj";
+coursesButtonElement.addEventListener("click", () => {
+    education.classList.add("article--hidden");
+    courses.classList.toggle("article--hidden");
+    skils.classList.add("article--hidden");
+    interested.classList.add("article--hidden");
+    educationButtonName.innerText = education.classList.contains("article--hidden") ? "Education" : "Ukryj";
+    coursesButtonName.innerText = courses.classList.contains("article--hidden") ? "Courses" : "Ukryj";
+    skilsButtonName.innerText = skils.classList.contains("article--hidden") ? "Skils" : "Ukryj";
+    interestedButtonName.innerText = interested.classList.contains("article--hidden") ? "Interested" : "Ukryj";
 });
 
-section__buttonSkils.addEventListener("click", () => {
-    section__education.classList.add("section__hidden");
-    section__courses.classList.add("section__hidden");
-    section__skils.classList.toggle("section__hidden");
-    section__interested.classList.add("section__hidden");
-    section__buttonEducationName.innerText = section__education.classList.contains("section__hidden") ? "Education" : "Ukryj";
-    section__buttonCoursesName.innerText = section__courses.classList.contains("section__hidden") ? "Courses" : "Ukryj";
-    section__buttonSkilsName.innerText = section__skils.classList.contains("section__hidden") ? "Skils" : "Ukryj";
-    section__buttonInterestedName.innerText = section__interested.classList.contains("section__hidden") ? "Interested" : "Ukryj";
+skilsButtonElement.addEventListener("click", () => {
+    education.classList.add("article--hidden");
+    courses.classList.add("article--hidden");
+    skils.classList.toggle("article--hidden");
+    interested.classList.add("article--hidden");
+    educationButtonName.innerText = education.classList.contains("article--hidden") ? "Education" : "Ukryj";
+    coursesButtonName.innerText = courses.classList.contains("article--hidden") ? "Courses" : "Ukryj";
+    skilsButtonName.innerText = skils.classList.contains("article--hidden") ? "Skils" : "Ukryj";
+    interestedButtonName.innerText = interested.classList.contains("article--hidden") ? "Interested" : "Ukryj";
 });
 
-section__buttonInterested.addEventListener("click", () => {
-    section__education.classList.add("section__hidden");
-    section__courses.classList.add("section__hidden");
-    section__skils.classList.add("section__hidden");
-    section__interested.classList.toggle("section__hidden");
-    section__buttonEducationName.innerText = section__education.classList.contains("section__hidden") ? "Education" : "Ukryj";
-    section__buttonCoursesName.innerText = section__courses.classList.contains("section__hidden") ? "Courses" : "Ukryj";
-    section__buttonSkilsName.innerText = section__skils.classList.contains("section__hidden") ? "Skils" : "Ukryj";
-    section__buttonInterestedName.innerText = section__interested.classList.contains("section__hidden") ? "Interested" : "Ukryj";
+interestedButtonElement.addEventListener("click", () => {
+    education.classList.add("article--hidden");
+    courses.classList.add("article--hidden");
+    skils.classList.add("article--hidden");
+    interested.classList.toggle("article--hidden");
+    educationButtonName.innerText = education.classList.contains("article--hidden") ? "Education" : "Ukryj";
+    coursesButtonName.innerText = courses.classList.contains("article--hidden") ? "Courses" : "Ukryj";
+    skilsButtonName.innerText = skils.classList.contains("article--hidden") ? "Skils" : "Ukryj";
+    interestedButtonName.innerText = interested.classList.contains("article--hidden") ? "Interested" : "Ukryj";
 });    
